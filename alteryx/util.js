c493f6189683552b7864d60139f52ae574a36ec3
PWCNZ\tschubi <tim.p.schubiger@nz.pwc.com>
2019-02-14 16:35:44 +1300

    Added async example

--- alteryx/util.js
+++ alteryx/util.js
@@ -1,7 +1,39 @@
 const alteryxRequest = require('./connection');
 
+const timeout = (s) => new Promise(resolve => setTimeout(resolve, s * 1000));
+
+
+const checkJobStatus = async (jobId, intervalInSec) => {
+
+    let jobDetail;
+
+    do {
+        console.log("request")
+        let response = await Promise.all([
+            alteryxRequest("GET", `jobs/${jobId}/`),
+            timeout(intervalInSec)
+        ])
+
+        jobDetail = response[0];
+    }
+    while (jobDetail.status == 'Running');
+
+    return (jobDetail)
+};
+
+
 module.exports = {
 
     getWorkflows: () => alteryxRequest("GET", "workflows/subscription/"),
+
+    queueWorkflow: (appId) => alteryxRequest("POST", `workflows/${appId}/jobs/`),
+
+    queueWorkflowAsync: (appId, intervalInSec) => {
+        return alteryxRequest("POST", `workflows/${appId}/jobs/`).then(response => {
+           return checkJobStatus(response.id, intervalInSec)
+        })
+    },
+
+    getJob: (jobId) => alteryxRequest("GET", `jobs/${jobId}/`),
 
 };
--- run.js
+++ run.js
@@ -3,5 +3,7 @@ require('dotenv').config();
 
 const alteryxUtil = require('./alteryx/util');
 
-    
-alteryxUtil.getWorkflows().then(response => console.log(response)).catch(err => console.log(err))
+alteryxUtil.queueWorkflowAsync("5c64beda2fc61232e0817bd6", 1).then(response => {
+    console.log(response);
+});
+
